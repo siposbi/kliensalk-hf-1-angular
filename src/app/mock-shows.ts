e68514f1e2044865013701f394d0b0a9cc46057b
Balázs Sipos <thesiposthe@gmail.com>
2021-03-16 12:14:22 +0100

    Added mock list

---
+++ src/app/mock-shows.ts
@@ -0,0 +1,9 @@
+import {Show} from './Show';
+
+export const SHOWS: Show[] = [
+  {title: 'Dummy Title 1', releaseDate: new Date(), imdbUrl: 'imdburl', netflixUrl: 'netflixurl', myRating: 5},
+  {title: 'Dummy Title 2', releaseDate: new Date(), imdbUrl: 'imdburl', netflixUrl: 'netflixurl', myRating: 4},
+  {title: 'Dummy Title 3', releaseDate: new Date(), imdbUrl: 'imdburl', netflixUrl: 'netflixurl', myRating: 3},
+  {title: 'Dummy Title 4', releaseDate: new Date(), imdbUrl: 'imdburl', netflixUrl: 'netflixurl', myRating: 2},
+  {title: 'Dummy Title 5', releaseDate: new Date(), imdbUrl: 'imdburl', netflixUrl: 'netflixurl', myRating: 1}
+];
--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Show} from '../Show';
+import {Component, OnInit} from '@angular/core';
+import {SHOWS} from '../mock-shows';
 
 @Component({
   selector: 'app-shows',
@@ -7,15 +7,10 @@ import {Show} from '../Show';
   styleUrls: ['./shows.component.css']
 })
 export class ShowsComponent implements OnInit {
-  show: Show = {
-    title: 'You',
-    releaseDate: new Date(2018, 9, 9),
-    imdbUrl: 'https://www.imdb.com/title/tt7335184',
-    netflixUrl: 'https://www.netflix.com/title/80211991',
-    myRating: 5
-  };
+  shows = SHOWS;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
