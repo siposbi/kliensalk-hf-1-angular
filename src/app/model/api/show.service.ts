b2bdd5613a25adc162f6d3851431c9a24a1d2357
Balázs Sipos <thesiposthe@gmail.com>
2021-03-17 22:43:30 +0100

    Removed comments.

--- src/app/model/api/show.service.ts
+++ src/app/model/api/show.service.ts
@@ -34,10 +34,8 @@ export class ShowService {
   private handleError<T>(operation = 'operation', result?: T): any {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+      console.error(error);
 
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
